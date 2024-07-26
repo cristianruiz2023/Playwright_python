74a30a54726f7c135a6b262fec4907f4b882c610
Cristian Ruiz <cristianruizvalerod@gmail.com>
2024-07-26 10:30:57 +0200

    practica input2

---
+++ input2.py
@@ -0,0 +1,75 @@
+import re
+import time
+from playwright.sync_api import Page, expect, Playwright, sync_playwright
+
+#comando exe pytest --slowmo 750 --headed input2.py
+#FUNCION PRINCIPAL DEL TEST
+#def test_input2(page: Page):
+def test_input2(playwright: Playwright) -> None:
+    # Inicia el navegador Chromium en modo no headless (mostrando la ventana) con un retraso 500ms
+    browser=playwright.chromium.launch(headless=False, slow_mo=500)
+    # Crea un nuevo contexto de navegador con un tamaño de ventana específico
+    #  configurado para grabar video
+    context=browser.new_context(
+        viewport={'width':1500, 'height':800},
+        record_video_dir="videos/input2"
+    )
+    # Crea una nueva página en el contexto del navegador
+    page=context.new_page()
+    #page.set_viewport_size({'width':500, 'height':400})
+
+     # Navega a la URL especificada
+    page.goto("https://demoqa.com/text-box")
+    # valida el titulo de la url
+    expect(page).to_have_title("DEMOQA")
+
+    #Tiempo de espera
+    page.set_default_timeout(4000)
+
+    # Localiza el campo de entrada de nombre de usuario
+    nombre=page.locator("#userName")
+    # Verifica que el campo de entrada de nombre de usuario sea visible
+    expect(nombre).to_be_visible()
+    # Llena el campo de entrada de nombre de usuario con "Cristian Ruiz"
+    page.locator("#userName").fill("Cristian Ruiz")
+    # Toma una captura de pantalla del campo de entrada de nombre de usuario
+    page.screenshot(path="capturas/input2/username.png")
+    # Localiza el campo de entrada de correo electrónico
+    email=page.locator("//input[contains(@id,'userEmail')]")
+    # Verifica que el campo de entrada de correo electrónico esté habilitado
+    expect(email).to_be_enabled()
+    # Verifica que el campo de entrada de correo electrónico esté vacío
+    expect(email).to_be_empty()
+    # Verifica que el campo de entrada de correo electrónico tenga el ID 'userEmail'
+    expect(email).to_have_id('userEmail')
+     # Llena el campo de entrada de correo electrónico con "Cristian@ruiz.com"
+    page.locator("//input[contains(@id,'userEmail')]").fill("Cristian@ruiz.com")
+    # Toma una captura de pantalla del campo de entrada de correo electrónico
+    page.screenshot(path="capturas/input2/useremail.png")
+    #tiempo de espera forzado
+    #time.sleep(2)#espera 2 segundos
+    # Llena el campo de dirección actual
+    page.locator('//*[@id="currentAddress"]').fill("address uno")
+     # Llena el campo de dirección permanente
+    page.locator('#permanentAddress').fill("address dos")
+
+    #validar si submit esta visible
+    button=page.locator('//*[@id="submit"]')
+    expect(button).to_be_visible()
+    # Si el botón está presente, haz clic en él
+    if button:
+        page.locator('//*[@id="submit"]').click()
+    else:
+        # Si el botón no se encuentra, imprime un mensaje
+        print("No se encontro el boton")
+     #Verifica que la URL de la página sea la esperada después del envío
+    expect(page).to_have_url("https://demoqa.com/text-box")
+    #variable del resumen con su id
+    resumen=page.locator("#email")
+    #validar el resumen contenga el texto 'Email'
+    expect(resumen).to_contain_text("Email:")
+
+    # Cierra el contexto del navegador y el navegador
+    context.close()
+    browser.close()
+    
--- test_dos.py
+++ test_dos.py
@@ -9,36 +9,36 @@ def test_dos(page: Page):
 
     #button hamburger
     page.locator('text=Elements').click()
-    page.screenshot(path='capturas/test_dos_button_hamb.png')
+    page.screenshot(path='capturas/test_dos/_button_hamb.png')
     #validar end point
     expect(page).to_have_url(re.compile(".*elements"))
 
     # button text box
 
     page.locator('text=Text Box').click()
-    page.screenshot(path='capturas/test_dos_button_text_box.png')
+    page.screenshot(path='capturas/test_dos/_button_text_box.png')
     # validar endpoint relativo del form
     expect(page).to_have_url(re.compile(".*/text-box"))
 
     # text box full name
 
     page.locator('#userName').fill('cristian ruiz')
-    page.screenshot(path='capturas/test_dos_text_box_full_name.png')
+    page.screenshot(path='capturas/test_dos/_text_box_full_name.png')
 
     # text box email
 
     page.locator('#userEmail').fill('cristian@ruiz.com')
-    page.screenshot(path='capturas/test_dos_text_box_email.png')
+    page.screenshot(path='capturas/test_dos/_text_box_email.png')
 
     #current adreess
     page.locator("//TEXTAREA[@id='currentAddress']").fill('adreess uno')
-    page.screenshot(path='capturas/test_dos_text_box_curadreess.png')
+    page.screenshot(path='capturas/test_dos/_text_box_curadreess.png')
     #permanent adreess
     page.locator('#permanentAddress').fill('adreess dos')
-    page.screenshot(path='capturas/test_dos_text_box_permadress.png')
+    page.screenshot(path='capturas/test_dos/_text_box_permadress.png')
     #button submit
     page.locator('#submit').click()
-    page.screenshot(path='capturas/test_dos_button_submit.png')
+    page.screenshot(path='capturas/test_dos/_button_submit.png')
     expect(page).to_have_url(re.compile(".*/text-box"))
 
     
